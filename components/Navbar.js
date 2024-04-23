39a7f01f9c228fc0ebb059ae0fc847e854ee332a
alphanse98 <alphanse98@gmail.com>
2024-04-24 04:26:03 +0530

    add icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import SearchIcon from "../asset/searchIcon";
+// import SearchIcon from "../asset/searchIcon";
 import Logo from "../asset/Logo";
 import { HeartIcon } from "../asset/HeartIcon";
 import BagIcon from "../asset/BagIcon";
-import ProfileIcon from "../asset/Profileicon";
+// import ProfileIcon from "../asset/Profileicon";
 
 const NavBar = () => {
   return (
@@ -14,10 +14,70 @@ const NavBar = () => {
         </div>
         <div className="text-3xl font-bold">LOGO</div>
         <div className="flex gap-5">
-          <SearchIcon />
+          {/* <SearchIcon /> */}
+          <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="vuesax/linear/search-normal">
+        <g id="vuesax/linear/search-normal_2">
+          <g id="search-normal">
+            <path
+              id="Vector"
+              d="M11.5 20.9998C16.7467 20.9998 21 16.7465 21 11.4998C21 6.25311 16.7467 1.99982 11.5 1.99982C6.25329 1.99982 2 6.25311 2 11.4998C2 16.7465 6.25329 20.9998 11.5 20.9998Z"
+              stroke="#292D32"
+              stroke-width="1.40625"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              id="Vector_2"
+              d="M22 21.9998L20 19.9998"
+              stroke="#292D32"
+              stroke-width="1.40625"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
           <HeartIcon />
           <BagIcon />
-          <ProfileIcon />
+          {/* <ProfileIcon /> */}
+          <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="vuesax/twotone/profile">
+        <g id="vuesax/twotone/profile_2">
+          <g id="profile">
+            <path
+              id="Vector"
+              d="M12.1625 10.87C12.0625 10.86 11.9425 10.86 11.8325 10.87C9.4525 10.79 7.5625 8.84 7.5625 6.44C7.5625 3.99 9.5425 2 12.0025 2C14.4525 2 16.4425 3.99 16.4425 6.44C16.4325 8.84 14.5425 10.79 12.1625 10.87Z"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              id="Vector_2"
+              d="M7.15875 14.56C4.73875 16.18 4.73875 18.82 7.15875 20.43C9.90875 22.27 14.4188 22.27 17.1688 20.43C19.5888 18.81 19.5888 16.17 17.1688 14.56C14.4288 12.73 9.91875 12.73 7.15875 14.56Z"
+              stroke="#292D32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
         </div>
       </div>
 
